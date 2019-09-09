85b1d4252526c2a4c239cba8e52b072f8c749caa
Alberto Gomez <git@albertogomez.dev>
2019-09-09 18:39:50 +0200

    je

--- src/main.ts
+++ src/main.ts
@@ -10,42 +10,43 @@ const app = express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
-app.get(
-  ['/template', '/template/index.html'],
-  async (req: Request, res: Response) => {
-    const data = {
-      date: 'Jueves, 19 de Septiembre de 2019',
-      talks: [
-        {
-          title: 'Asdasdasd',
-          avatar: 'https://i.imgur.com/Sm7xEPo.png',
-          speaker: 'Alberto Gómez',
-          twitter: '@gocastilla',
-          rank: 'Front-end developer en Coderty'
-        },
-        {
-          title: 'Asdasdasd',
-          avatar: 'https://i.imgur.com/Sm7xEPo.png',
-          speaker: 'José Manuel García Giménez',
-          twitter: '@josefdsa',
-          rank: 'Cloud Data engineer en Elastacloud'
-        }
-      ]
-    };
-    ejs.renderFile(
-      path.resolve(__dirname, '../template/index.html'),
-      data,
-      {},
-      (error, html) => {
-        if (error) {
-          res.send(error);
-        } else {
-          res.send(html);
-        }
-      }
-    );
-  }
+app.get('/template', (req: Request, res: Response) =>
+  res.redirect('/template/index.html')
 );
+
+app.get('/template/index.html', async (req: Request, res: Response) => {
+  const data = {
+    date: 'Jueves, 19 de Septiembre de 2019',
+    talks: [
+      {
+        title: 'Asdasdasd',
+        avatar: 'https://i.imgur.com/Sm7xEPo.png',
+        speaker: 'Alberto Gómez',
+        twitter: '@gocastilla',
+        rank: 'Front-end developer en Coderty'
+      },
+      {
+        title: 'Asdasdasd',
+        avatar: 'https://i.imgur.com/Sm7xEPo.png',
+        speaker: 'José Manuel García Giménez',
+        twitter: '@josefdsa',
+        rank: 'Cloud Data engineer en Elastacloud'
+      }
+    ]
+  };
+  ejs.renderFile(
+    path.resolve(__dirname, '../template/index.html'),
+    data,
+    {},
+    (error, html) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(html);
+      }
+    }
+  );
+});
 
 app.use('/template', express.static(path.resolve(__dirname, '../template')));
 
