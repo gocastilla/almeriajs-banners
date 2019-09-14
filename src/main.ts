2603291fb905fa35de93c7b1e516f4a4e10bd7ac
Alberto Gómez Castilla <alberto@MacBook-Pro-de-Alberto.local>
2019-09-14 19:34:44 +0200

    g

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import express, {
   Request as Req,
   Response as Res,
   NextFunction as Next
-} from 'express';
-import BodyParser from 'body-parser';
-import cors from 'cors';
-import path from 'path';
+} from "express";
+import BodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
 
-import { takeScreenshot } from './example-puppeteer';
-import { renderTemplate } from './example-ejs';
+import { takeScreenshot } from "./example-puppeteer";
+import { renderTemplate } from "./example-ejs";
 
 const PORT = 4444;
 
@@ -17,21 +17,28 @@ app.use(cors());
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
-app.use('/', express.static(path.resolve(__dirname, '../form')));
+app.use("/", express.static(path.resolve(__dirname, "../form")));
 
-app.get('/template/index.html', (req: Req, res: Res) => {
+app.get("/template/index.html", (req: Req, res: Res) => {
   const data = req.query.data ? JSON.parse(req.query.data) : {};
-  renderTemplate(path.resolve(__dirname, '../template/index.html'), data)
+  renderTemplate(path.resolve(__dirname, "../template/index.html"), data)
     .then(template => res.send(template))
     .catch(error => res.status(500).send(error));
 });
 
-app.use('/template', express.static(path.resolve(__dirname, '../template')));
+app.use("/template", express.static(path.resolve(__dirname, "../template")));
 
-app.post('/banner', (req: Req, res: Res) => {
+app.post("/banner", (req: Req, res: Res) => {
   const data = encodeURI(JSON.stringify(req.body || {}));
   takeScreenshot(`http://localhost:${PORT}/template/index.html?data=${data}`)
-    .then(image => res.contentType('image/png').send(image))
+    .then(image => res.contentType("image/png").send(image))
+    .catch(error => res.status(500).send(error));
+});
+
+app.get("/banner", (req: Req, res: Res) => {
+  const data = encodeURI(JSON.stringify(req.body || {}));
+  takeScreenshot(`http://localhost:${PORT}/template/index.html?data=${data}`)
+    .then(image => res.contentType("image/png").send(image))
     .catch(error => res.status(500).send(error));
 });
 
